620b15905cf77dd4268795188bd2ea5cc4438614
Pushpal Ghoshal <pushpal.ghoshal@rediffmail.com>
2021-05-04 22:06:36 +0530

    Completed sticky header functionality

--- app/assets/Scripts/App.js
+++ app/assets/Scripts/App.js
@@ -1,11 +1,18 @@
 import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
-import RevealOnscroll from './modules/RevealOnScroll(new)'
+import RevealOnscroll from './modules/RevealOnScroll'
+import StickyHeader from './modules/StickyHeader'
 
+let stickyHeader = new StickyHeader();
 const mobileMenu = new MobileMenu();
 new RevealOnscroll(document.querySelectorAll(".feature-item"),75);
 new RevealOnscroll(document.querySelectorAll(".testimonial"),60);
 
+let testimonialLink = document.querySelector("#testimonials-link")
+
+testimonialLink.addEventListener('click',() => {
+    testimonialLink.classList.add("is-current-link")
+})
 
 if(module.hot){
     module.hot.accept();
