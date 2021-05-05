a0d09fb0bc7da533ab4619d78c59b7fb4511887a
Pushpal Ghoshal <pushpal.ghoshal@rediffmail.com>
2021-05-05 15:27:30 +0530

    Completed the modal overlay

--- app/assets/Scripts/App.js
+++ app/assets/Scripts/App.js
@@ -2,19 +2,36 @@ import '../styles/styles.css'
 import MobileMenu from './modules/MobileMenu'
 import RevealOnscroll from './modules/RevealOnScroll'
 import StickyHeader from './modules/StickyHeader'
+//import Modal from './modules/Modal'
 
+//new Modal;
 let stickyHeader = new StickyHeader();
 const mobileMenu = new MobileMenu();
-new RevealOnscroll(document.querySelectorAll(".feature-item"),75);
-new RevealOnscroll(document.querySelectorAll(".testimonial"),60);
-
+new RevealOnscroll(document.querySelectorAll(".feature-item"), 75);
+new RevealOnscroll(document.querySelectorAll(".testimonial"), 60);
 let testimonialLink = document.querySelector("#testimonials-link")
+let modal
 
-testimonialLink.addEventListener('click',() => {
+
+testimonialLink.addEventListener('click', () => {
     testimonialLink.classList.add("is-current-link")
 })
 
-if(module.hot){
+document.querySelectorAll(".open-modal").forEach(el => {
+  el.addEventListener("click", e => {
+    e.preventDefault()
+    if (typeof modal == "undefined") {
+      import(/* webpackChunkName: "modal" */ './modules/Modal').then(x => {
+        modal = new x.default()
+        setTimeout(() => modal.openTheModal(), 20)
+      }).catch(() => console.log("There was a problem."))
+    } else {
+      modal.openTheModal()
+    }
+  })
+})
+
+if (module.hot) {
     module.hot.accept();
 }
 
