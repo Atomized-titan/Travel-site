d3c3d465d0aec2c6649e152f82dbd6355631a0a9
Pushpal Ghoshal <pushpal.ghoshal@rediffmail.com>
2021-05-05 23:38:44 +0530

    webpack-config tweak

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ let pages = fse.readdirSync('./app').filter(function(file) {
 })
 
 let config = {
-  entry: './app/assets/scripts/App.js',
+  entry: path.resolve(__dirname, 'src') + './app/assets/scripts/App.js',
   plugins: pages,
   module: {
     rules: [
